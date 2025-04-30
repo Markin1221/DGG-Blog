237d7a1c099b3fc652b24b837bec9e4d2326c6b5
arnaldojse <arnaldojoseboy@gmail.com>
2025-04-29 21:15:13 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main/login/script.js
+++ main/login/script.js
@@ -1,12 +1,20 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loguinBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+document.getElementById('mudar-tema').addEventListener('click', function() {
+    event.preventDefault();
+   document.body.classList.toggle('light-mode');
+   document.querySelector('header').classList.toggle('light-mode');
+   document.querySelectorAll('.postagem').forEach(post => {
+        post.classList.toggle('light-mode');    
+    });
+    document.querySelectorAll('.PostCont').forEach(cont => {
+        cont.classList.toggle('light-mode');
+    });
+    document.querySelectorAll('.a').forEach(link => {
+        link.classList.toggle('light-mode');
+    });
+    document.querySelectorAll('.h6').forEach(h6 => {
+        h6.classList.toggle('light-mode');
+    });
+    document.querySelectorAll('.bolinha').forEach(bolinha => {
+        bolinha.classList.toggle('light-mode');
+    });
 });
-
-loguniBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
