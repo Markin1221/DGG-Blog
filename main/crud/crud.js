cfdf61817684c41f929e6df57c76a3a5b087fa6a
parzi18 <pedromv2006@gmail.com>
2025-05-20 12:28:12 -0300

    sla
    smt

--- main/crud/crud.js
+++ main/crud/crud.js
@@ -228,3 +228,41 @@
   // Invoca a inicialização quando o script carregar
   init();
 })();
+
+function mostrarCadastro() {
+  const loginForm = document.getElementById("login-form");
+  const cadastroForm = document.getElementById("cadastro-form");
+
+  // some com o login com fadeOut
+  loginForm.style.animation = "fadeOut 0.4s ease forwards";
+
+  setTimeout(() => {
+    loginForm.classList.remove("visivel");
+    loginForm.classList.add("oculto");
+
+    cadastroForm.classList.remove("oculto");
+    cadastroForm.classList.add("visivel");
+    cadastroForm.style.animation = "fadeIn 0.4s ease forwards";
+  }, 400);
+}
+
+function mostrarLogin() {
+  const loginForm = document.getElementById("login-form");
+  const cadastroForm = document.getElementById("cadastro-form");
+
+  // some com o cadastro com fadeOut
+  cadastroForm.style.animation = "fadeOut 0.4s ease forwards";
+
+  setTimeout(() => {
+    cadastroForm.classList.remove("visivel");
+    cadastroForm.classList.add("oculto");
+
+    loginForm.classList.remove("oculto");
+    loginForm.classList.add("visivel");
+    loginForm.style.animation = "fadeIn 0.4s ease forwards";
+  }, 400);
+}
+function entrar() {
+  // Aqui você pode colocar validação se quiser
+  window.location.href = "../index.html"; // Altere o nome conforme sua estrutura
+}
